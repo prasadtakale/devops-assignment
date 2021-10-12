cbf6313e346b765f6a41c547a4d29fa0a0dca293
prasadtakale <yatintakale@gmail.com>
2021-10-13 01:23:00 +0530

    Added hostname to flask app

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
+import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!<br>I am using docker'
+    return 'Hello, World!<br>I am using docker ' + socket.gethostname()
